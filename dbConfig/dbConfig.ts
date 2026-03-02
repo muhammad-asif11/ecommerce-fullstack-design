476c76e85233dd2e6ec091b4170b565af5f8a0c0
M Asif <74290817+muhammad-asif11@users.noreply.github.com>
2026-03-02 13:49:42 +0500

    Update dbConfig.ts
    missing awaite in db connection

--- dbConfig/dbConfig.ts
+++ dbConfig/dbConfig.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connect() {
   try {
-    mongoose.connect(process.env.MONGODB_URI!);
+    await mongoose.connect(process.env.MONGODB_URI as string);
     const connection = mongoose.connection;
     // mongodb event
     connection.on("connected", () => {
