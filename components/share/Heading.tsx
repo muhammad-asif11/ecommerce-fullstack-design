2b4c7612976086281805d3364a3dd9a9d7ea8d9b
muhammad-asif11 <biz.asif11@gmail.com>
2026-03-03 00:28:11 +0500

    fix width of borderline componentl

--- components/share/Heading.tsx
+++ components/share/Heading.tsx
@@ -4,7 +4,7 @@ type Props = {
 };
 const Heading = ({ title }: Props) => {
   return (
-    <article className="w-full">
+    <article className="w-full sm:w-fit">
       <h2 className="text-xl sm:text-3xl font-semibold">{title}</h2>
     </article>
   );
